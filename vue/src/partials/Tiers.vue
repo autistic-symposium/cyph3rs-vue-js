95dedf0b40980d5d6445475472a685d6830bec89
bt3gl <bt3@protonmail.ch>
2022-12-14 09:24:05 -0800

    earn from sahring your experience

--- vue/src/partials/Tiers.vue
+++ vue/src/partials/Tiers.vue
@@ -63,7 +63,7 @@
                 <svg class="w-3 h-3 fill-current text-emerald-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                   <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                 </svg>
-                <span>Connect with other searcher operators</span>
+                <span>Connect with other searcher operators and earn from sharing your experience</span>
               </li>
               <li class="flex items-center">
                 <svg class="w-3 h-3 fill-current text-emerald-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
