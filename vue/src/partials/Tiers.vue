347c3e85a1ee0c7d39266f3aa8c14e3a929d43dd
mvonsteinkirch <bt3@protonmail.ch>
2023-01-11 16:11:17 -0800

    💾

--- vue/src/partials/Tiers.vue
+++ vue/src/partials/Tiers.vue
@@ -151,7 +151,7 @@
                   <path
                     d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                 </svg>
-                <span>Access to strategies research and
+                <span>Gain access to strategies research and
                   analysis of retired bots</span>
               </li>
 
@@ -161,7 +161,7 @@
                   <path
                     d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                 </svg>
-                <span>Browse bounties and get matched to projects</span>
+                <span>Browse bounties where we'll match you to projects</span>
               </li>
               <li class="flex items-center">
                 <svg class="w-3 h-3 fill-current text-emerald-500 mr-3 shrink-0" viewBox="0 0 12 12"
