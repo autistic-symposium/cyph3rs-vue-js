b482f2d09936575ead451ad20e71f8537bb2160a
mvonsteinkirch <bt3@protonmail.ch>
2023-01-12 12:58:08 -0800

    💾

--- vue/src/partials/Tiers.vue
+++ vue/src/partials/Tiers.vue
@@ -18,7 +18,7 @@
             <p class="text-xl text-gray-400 font-bold">
           
               Whether you are just starting the MEV game, an experienced searcher operator, or an application-layer MEV
-              protocol, we are here for you
+              protocol, Cyph3rs is the marketplace for you
           </p>
           </div>
 
