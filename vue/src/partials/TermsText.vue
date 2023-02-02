44bd3e5e7e88220509d0a57168eb4580760b47dd
mvonsteinkirch <bt3@protonmail.ch>
2023-02-02 09:49:55 -0800

    💾

--- vue/src/partials/TermsText.vue
+++ vue/src/partials/TermsText.vue
@@ -12,11 +12,6 @@
           <h2 class="h2 font-sora" data-aos="zoom-out">Terms of Service - Cyph3rs</h2>
         </div>
 
-        <div class="relative max-w-6xl mx-auto px-4 sm:px-6 text-green-1 font-bold">
-          <p class="pb-12">
-            Last updated: 14 December 2022.
-          </p>
-        </div>
 
         <div class="relative max-w-6xl mx-auto px-4 sm:px-6">
           <h4 class="h4 text-gray-200  pb-12">Yes, this is a joke! But it would be cool, wouldn't it? 😉</h4>
